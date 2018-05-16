678665f17ca19c0d08bf9b1841b7ed996436b012
yasui05821 <yasui05821@gmail.com>
2018-05-16 16:18:42 +0900

    webクリップアイコン追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,12 @@ module.exports = {
     title: 'wcosaka',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no'},
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel:'apple-touch-icon-precomposed', href: '/apple-touch-icon.png'}
     ]
   },
   /*
